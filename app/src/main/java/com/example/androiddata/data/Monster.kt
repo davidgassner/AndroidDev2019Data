d7daf2a59f8d9bd3204ec1325a2ef3cd23799252
David Gassner <dgassnerdev@gmail.com>
2019-06-24 10:32:20 -0700

    01_02e

---
+++ app/src/main/java/com/example/androiddata/data/Monster.kt
@@ -0,0 +1,10 @@
+package com.example.androiddata.data
+
+data class Monster (
+    val monsterName: String,
+    val imageFile: String,
+    val caption: String,
+    val description: String,
+    val price: Double,
+    val scariness: Int
+)
--- app/src/main/java/com/example/androiddata/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/androiddata/ui/main/MainFragment.kt
@@ -1,12 +1,14 @@
 package com.example.androiddata.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.androiddata.R
+import com.example.androiddata.data.Monster
 
 class MainFragment : Fragment() {
 
@@ -20,6 +22,11 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        val monster = Monster("Bob", "myfile", "a caption",
+            "a description", .19, 3)
+        Log.i("monsterLogging", monster.toString())
+
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
