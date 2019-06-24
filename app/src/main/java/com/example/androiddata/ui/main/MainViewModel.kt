9339cdbc68b86c727aca1d02518087b5af28e06d
David Gassner <dgassnerdev@gmail.com>
2019-06-24 11:13:31 -0700

    01_04e

--- app/src/main/java/com/example/androiddata/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/androiddata/ui/main/MainViewModel.kt
@@ -4,13 +4,13 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.example.androiddata.LOG_TAG
-import com.example.androiddata.R
 import com.example.androiddata.utilities.FileHelper
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
 
     init {
-        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
+//        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
+        val text = FileHelper.getTextFromAssets(app, "monster_data.json")
         Log.i(LOG_TAG, text)
     }
 }
--- app/src/main/java/com/example/androiddata/utilities/FileHelper.kt
+++ app/src/main/java/com/example/androiddata/utilities/FileHelper.kt
@@ -11,5 +11,13 @@ class FileHelper {
                 }
             }
         }
+
+        fun getTextFromAssets(context: Context, fileName: String): String {
+            return context.assets.open(fileName).use {
+                it.bufferedReader().use {
+                    it.readText()
+                }
+            }
+        }
     }
 }
