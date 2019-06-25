67c23988e7e1623fddbf252b764a7cbf3d22ff8a
David Gassner <dgassnerdev@gmail.com>
2019-06-24 19:55:53 -0700

    02_03e

--- app/src/main/java/com/example/androiddata/data/MonsterRepository.kt
+++ app/src/main/java/com/example/androiddata/data/MonsterRepository.kt
@@ -2,7 +2,10 @@ package com.example.androiddata.data
 
 import android.app.Application
 import android.content.Context
+import android.net.ConnectivityManager
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.example.androiddata.LOG_TAG
 import com.example.androiddata.utilities.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -18,6 +21,7 @@ class MonsterRepository(val app: Application) {
 
     init {
         getMonsterData()
+        Log.i(LOG_TAG, "Network available: ${networkAvailable()}")
     }
 
     fun getMonsterData() {
@@ -26,5 +30,13 @@ class MonsterRepository(val app: Application) {
         val adapter: JsonAdapter<List<Monster>> =
             moshi.adapter(listType)
         monsterData.value = adapter.fromJson(text) ?: emptyList()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun networkAvailable(): Boolean {
+        val connectivityManager = app.getSystemService(Context.CONNECTIVITY_SERVICE)
+            as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnectedOrConnecting ?: false
     }
 }
