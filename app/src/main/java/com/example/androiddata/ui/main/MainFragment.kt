77770d265e2885725e0d295423f29c0267062027
David Gassner <dgassnerdev@gmail.com>
2019-06-25 12:16:03 -0700

    03_03e

--- app/src/main/java/com/example/androiddata/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/androiddata/ui/main/MainFragment.kt
@@ -7,25 +7,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androiddata.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.monsterData.observe(this, Observer
         {
-
+            val adapter = MainRecyclerAdapter(requireContext(), it)
+            recyclerView.adapter = adapter
         })
 
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return view
     }
 
 }
---
+++ app/src/main/java/com/example/androiddata/ui/main/MainRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.androiddata.ui.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androiddata.R
+import com.example.androiddata.data.Monster
+
+class MainRecyclerAdapter(val context: Context,
+                          val monsters: List<Monster>):
+    RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder>()
+
+{
+    override fun getItemCount() = monsters.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.monster_grid_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val monster = monsters[position]
+        with(holder) {
+            nameText?.let {
+                it.text = monster.monsterName
+                it.contentDescription = monster.monsterName
+            }
+            ratingBar?.rating = monster.scariness.toFloat()
+        }
+    }
+
+    inner class ViewHolder(itemView: View) :
+            RecyclerView.ViewHolder(itemView) {
+        val nameText = itemView.findViewById<TextView>(R.id.nameText)
+        val monsterImage = itemView.findViewById<ImageView>(R.id.monsterImage)
+        val ratingBar = itemView.findViewById<RatingBar>(R.id.ratingBar)
+    }
+}
