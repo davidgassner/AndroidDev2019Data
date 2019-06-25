cceaf6e58916df9763e44c581f47c8a3b4e62432
David Gassner <dgassnerdev@gmail.com>
2019-06-24 20:31:20 -0700

    02_06e

--- app/src/main/java/com/example/androiddata/data/MonsterRepository.kt
+++ app/src/main/java/com/example/androiddata/data/MonsterRepository.kt
@@ -3,23 +3,36 @@ package com.example.androiddata.data
 import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
-import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.MutableLiveData
-import com.example.androiddata.LOG_TAG
-import com.example.androiddata.utilities.FileHelper
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
+import com.example.androiddata.WEB_SERVICE_URL
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MonsterRepository(val app: Application) {
 
     val monsterData = MutableLiveData<List<Monster>>()
 
     init {
-        getMonsterData()
+        CoroutineScope(Dispatchers.IO).launch {
+            callWebService()
+        }
     }
 
-    fun getMonsterData() {
+    @WorkerThread
+    suspend fun callWebService() {
+        if (networkAvailable()) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(WEB_SERVICE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+            val service = retrofit.create(MonsterService::class.java)
+            val serviceData = service.getMonsterData().body() ?: emptyList()
+            monsterData.postValue(serviceData)
+        }
     }
 
     @Suppress("DEPRECATION")
