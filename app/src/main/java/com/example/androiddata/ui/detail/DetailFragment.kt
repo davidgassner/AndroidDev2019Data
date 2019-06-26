9aa0554db87f38daba48e28d5c118b1bc60f4e37
David Gassner <dgassnerdev@gmail.com>
2019-06-26 11:10:56 -0700

    04_05e

--- app/src/main/java/com/example/androiddata/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/androiddata/ui/detail/DetailFragment.kt
@@ -2,19 +2,17 @@ package com.example.androiddata.ui.detail
 
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import com.example.androiddata.LOG_TAG
 import com.example.androiddata.R
+import com.example.androiddata.databinding.FragmentDetailBinding
 import com.example.androiddata.ui.shared.SharedViewModel
 
 /**
@@ -39,12 +37,14 @@ class DetailFragment : Fragment() {
         )
 
         viewModel = ViewModelProviders.of(requireActivity()).get(SharedViewModel::class.java)
+        val binding = FragmentDetailBinding.inflate(
+            inflater, container, false
+        )
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
-        viewModel.selectedMonster.observe(this, Observer {
-            Log.i(LOG_TAG, "Selected monster: ${it.monsterName}")
-        })
+        return binding.root
 
-        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
