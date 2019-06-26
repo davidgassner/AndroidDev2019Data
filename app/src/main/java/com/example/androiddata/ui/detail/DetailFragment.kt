f6c4d215d9851a2db180520f13336b2b9c182d4a
David Gassner <dgassnerdev@gmail.com>
2019-06-26 09:31:47 -0700

    04_03

---
+++ app/src/main/java/com/example/androiddata/ui/detail/DetailFragment.kt
@@ -0,0 +1,29 @@
+package com.example.androiddata.ui.detail
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androiddata.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        (requireActivity() as AppCompatActivity).run {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+
+}
