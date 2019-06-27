71f567a79fd1fe741c4eb0239d8f458a32ad1849
David Gassner <dgassnerdev@gmail.com>
2019-06-27 13:31:32 -0700

    05_04e

--- app/src/main/java/com/example/androiddata/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/androiddata/ui/splash/SplashFragment.kt
@@ -1,14 +1,20 @@
 package com.example.androiddata.ui.splash
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import com.example.androiddata.R
+
+const val PERMISSION_REQUEST_CODE = 1001
 
 class SplashFragment : Fragment() {
 
@@ -16,21 +22,52 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        displayMainFragment()
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            displayMainFragment()
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    fun displayMainFragment() {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>, grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0]
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                displayMainFragment()
+            } else {
+                Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+    }
+
+    private fun displayMainFragment() {
         val navController = Navigation.findNavController(
             requireActivity(), R.id.nav_host
         )
-        navController.navigate(R.id.action_nav_main,
-            null,
+        navController.navigate(
+            R.id.action_nav_main, null,
             NavOptions.Builder()
                 .setPopUpTo(R.id.splashFragment, true)
                 .build()
-            )
+        )
     }
 
 }
