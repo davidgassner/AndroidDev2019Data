62e9fb885b6bc9758b989c5bac877c6e07604e88
David Gassner <dgassnerdev@gmail.com>
2019-06-27 12:27:16 -0700

    05_03e

---
+++ app/src/main/java/com/example/androiddata/ui/splash/SplashFragment.kt
@@ -0,0 +1,36 @@
+package com.example.androiddata.ui.splash
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import com.example.androiddata.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        displayMainFragment()
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    fun displayMainFragment() {
+        val navController = Navigation.findNavController(
+            requireActivity(), R.id.nav_host
+        )
+        navController.navigate(R.id.action_nav_main,
+            null,
+            NavOptions.Builder()
+                .setPopUpTo(R.id.splashFragment, true)
+                .build()
+            )
+    }
+
+}
