952798fcc1b1eca592a5f6077ac3f8ebfbfb7518
David Gassner <dgassnerdev@gmail.com>
2019-07-05 10:28:40 -0700

    06_01e

--- app/src/main/java/com/example/androiddata/data/Monster.kt
+++ app/src/main/java/com/example/androiddata/data/Monster.kt
@@ -1,8 +1,13 @@
 package com.example.androiddata.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.androiddata.IMAGE_BASE_URL
 
+@Entity(tableName = "monsters")
 data class Monster (
+    @PrimaryKey(autoGenerate = true)
+    val monsterId: Int,
     val monsterName: String,
     val imageFile: String,
     val caption: String,
